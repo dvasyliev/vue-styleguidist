93bfd19646c54f8ce2ec79b38b26429897c2b780
elevatebart <ledouxb@me.com>
2019-08-18 11:07:42 -0500

    refactor: enable typescript

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,18 @@
 module.exports = {
 	presets: [
 		[
-			'@babel/preset-env',
+			'@babel/env',
 			{
 				targets: {
 					chrome: 59,
 					ie: 11
 				},
-				forceAllTransforms: true,
-				modules: false
+				forceAllTransforms: true
+				//modules: false
 			}
 		],
-		'@babel/preset-react'
+		'@babel/react',
+		'@babel/typescript'
 	],
 	plugins: [
 		'@babel/plugin-syntax-dynamic-import',
@@ -20,7 +21,7 @@ module.exports = {
 	],
 	env: {
 		test: {
-			presets: ['@babel/preset-env', '@babel/preset-react'],
+			presets: ['@babel/env', '@babel/react'],
 			plugins: [
 				'@babel/plugin-syntax-dynamic-import',
 				'@babel/plugin-proposal-class-properties',
